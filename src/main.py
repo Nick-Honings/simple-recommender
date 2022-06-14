3db070c62ebd70e4909accdc3771295986bbf6e1
Nick Honings <i409398@fhict.nl>
2022-06-14 22:42:50 +0200

    faas

--- src/main.py
+++ src/main.py
@@ -6,13 +6,13 @@ from helpers import UIHelper
 data_config = {
     'review_data_path': '../dataset/Video_Games_k5_cluster.json.gz',
     'metadata_data_path': '../dataset/meta_Video_Games.json.gz',
-    'review_model_path': '../dataset/processed/average_product_rating_k5.pkl',
-    'metadata_model_path': '../dataset/processed/__metadata.pkl'
+    'review_model_path': '../models/average_product_rating_k5.pkl',
+    'metadata_model_path': '../models/metadata.pkl'
 }
 
 # always_train= True|False -> whether to always train dataset on startup
 def main(args):
-    title = args.get("title")
+    # title = args.get("title")
     recommender = SimpleRecommender(data_config)
     completion_event = threading.Event()
     print("Please wait a moment as I'm training myself")
@@ -22,19 +22,20 @@ def main(args):
     print("Training completed.")
     UIHelper.clear_console()
 
-    # video_game_title = input("What is your favourite video game: ")
+    video_game_title = input("What is your favourite video game: ")
 
     # Todo: get_recommendations
-    raw_recommendations = recommender.get_recommendations(title).tolist()
+    raw_recommendations = recommender.get_recommendations(video_game_title).tolist()
     video_recommendation = recommender.construct_recommended_video_games(raw_recommendations)
     print("I found these products in my collection:")
     for video in video_recommendation:
         print(video)
 
-    return { "body": video}
+    # return { "body": video}
 
 
-
+if __name__ == '__main__':
+    main("")
 
 
 
